12d789a9b3c37a4250d14ab9d6e85801e7f2bb7c
snarkusius <marcus@lengquist.se>
2020-11-02 15:50:36 +0100

    1st commit

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 // Read instructions. And remove this line.
 let input = document.getElementById("user-input")
-console.log(Math.floor(Math.random() * 50))
+let output = document.getElementById("outputtext")
+let guess = document.getElementById("Guess")
+let desplayHiskore = document.getElementById("message")
+let randomNumber = Math.round(Math.random() * 100)
+let array = []
+let hiskore = []
+if(window.localStorage.getItem('hiskore')>0){
+    hiskore.push (window.localStorage.getItem('hiskore'))
+    desplayHiskore.innerHTML = `Guess a number between 0 and 100 <br> hiskore (${Math.min(...hiskore)})`
+}
 
+    
+
+console.log(window.localStorage.getItem('hiskore'))
+console.log(randomNumber)
 document.addEventListener('keyup', function (event) {
     if(event.key == "Enter"){
-        console.log(input.value)
+        
+
+        if(input.value == randomNumber){
+            array.push(`<span style="color: #0000a0">${input.value}</span>`)
+            output.innerHTML = `korekt gess(${array.length})`
+            hiskore.push(array.length) 
+            reset()
+            window.localStorage.setItem("hiskore",Math.min(...hiskore).toString())
+        }
+        else{
+            if(input.value>randomNumber){
+                output.innerHTML = "lover"
+                array.push(`<span style="color: green">${input.value}</span>`)
+            }
+            else{
+                output.innerHTML = "higher"
+                array.push(`<span style="color: red">${input.value}</span>`)
+            }
+            
+        }
+        console.log(array)
+        guess.innerHTML = array.join("<br>") 
+        input.value = ""
     }})
+function reset(){
+    array = []
+    guess.innerHTML = array 
+    randomNumber = Math.round(Math.random() * 100)
+    console.log(randomNumber)
+    console.log(hiskore)
+    desplayHiskore.innerHTML = `Guess a number between 0 and 100 <br> hiskore (${Math.min(...hiskore)})`
+}
