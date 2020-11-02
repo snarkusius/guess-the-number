671e886e124b6eb308cf4ad4135759ccf9aef0ad
snarkusius <marcus@lengquist.se>
2020-11-02 13:48:08 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Read instructions. And remove this line.
+let input = document.getElementById("user-input")
+console.log(Math.floor(Math.random() * 50))
+
+document.addEventListener('keyup', function (event) {
+    if(event.key == "Enter"){
+        console.log(input.value)
+    }})
